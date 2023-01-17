03bb9d3fa7bf9e8cfa0a5aded9c59ed1c53bad25
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-01-16 20:45:45 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const validateButton = document.getElementById('validate-button');
+validateButton.addEventListener('click',validate);
+const results =  document.getElementById('result');
+
+function validate() {
+ const creditCardNumber = document.getElementById('card-number').value;
+ console.log(creditCardNumber);
+
+  const valid = isValid(creditCardNumber);
+  if (valid) { 
+    // Show success in div#result
+    results.innerHTML = "la tarjeta es valida";
+  }
+  else {
+    // Show error message in div#result
+    results.innerHTML = "la tarjeta no es valida";
+  }
+}
+
+function isValid(creditCardNumber){
+  // is creditCardNumber valid?
+  return true
+}
+
+
