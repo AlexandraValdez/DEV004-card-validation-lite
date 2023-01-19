da53ee633a6962d0cb24e5becfe6798bf39d446a
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-01-19 10:52:04 -0500

    Added foo to the bar

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
+const luhnAlgorithm = (ccNumber) => {
+  const length = ccNumber.length;
+  let count = 0;
 
+  //reverse the credit card number, if index (i) is even double the value, if above 9 then adjust value
+  for(let i = 0; i < length; i++) {
+    let currentDigit = parseInt(ccNumber[i]);
+    
+    if ((i+2) % 2 === 0)//para saber si es divisible por 2 y si hay 0 residuos
 
+      // si es divisible por 2 entonces se multiplica por 2 y si excede se resta por 9 que es el numero maximo
+      if ((currentDigit *= 2) > 9) currentDigit -= 9;
 
+    count += currentDigit;
 
-
-const validateButton = document.getElementById('validate-button');
-validateButton.addEventListener('click',validate);
-const results =  document.getElementById('result');
-
-function validate() {
-  const creditCardNumber = document.getElementById('card-number').value;
-  //console.log(creditCardNumber);
-
-  const valid = isValid(creditCardNumber);
-  if (valid) { 
-    // Show success in div#result
-    results.innerHTML = "la tarjeta es valida";
   }
-  else {
-    // Show error message in div#result
-    results.innerHTML = "la tarjeta no es valida";
-  }
+
+  return (count % 10) === 0;
+
 }
 
-function isValid(creditCardNumber){
-  // is creditCardNumber valid?
-  return true
-}
+// eslint-disable-next-line no-unused-vars
+const checkCC = () => {
+  const elCCNumber = document.getElementById('ccNumber');
+  const elCCValidation = document.getElementById('Validacion');
+  let message = "";
+
+  if( luhnAlgorithm(elCCNumber.value) )
+    message = "Funciono";
+  else
+    message = "La validacion de tarjeta no funciono";
+  elCCValidation.textContent = message;
+};
+
+
 
 
